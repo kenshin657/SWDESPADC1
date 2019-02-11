1d86eaebfbbe3e82e94ada52783d654d8bfc7a82
Kyle T <kyle_tagle@dlsu.edu.ph>
2019-02-11 17:45:10 +0800

    CSV READER NOW WORKING BUT NOT YET OUTPUTTING TO THE CALENDAR

--- designchallenge1/PSFRead.java
+++ designchallenge1/PSFRead.java
@@ -1,5 +1,10 @@
 package designchallenge1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PSFRead extends FileRead {
@@ -12,6 +17,46 @@ public class PSFRead extends FileRead {
     @Override
     List<Event> fileLoad() {
         System.out.println(super.filePath + "\n\n\n");
-        return null;
+
+        String psvFile = super.filePath;
+
+        BufferedReader br = null;
+        String line = "";
+        String psvSplit =" | ";
+
+        List<Event> eventList = new ArrayList<Event>(); //events received from csv file
+
+        try {
+            br = new BufferedReader(new FileReader(psvFile));
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                Event eventInfo = new Event();
+
+                String[] event = line.split(psvSplit);
+
+                eventInfo.setEventName(event[0]);
+                eventInfo.setDate(event[1]);
+                eventInfo.setTextColor(event[2]);
+
+                eventList.add(eventInfo);
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null)
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+        System.out.println(eventList.get(0).getEventName() + eventList.get(0).getDate());
+
+        return eventList;
     }
 }
